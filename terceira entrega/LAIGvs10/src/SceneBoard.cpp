ae93fd5f667fc970308caf2b923e0048e18dfc71
PMRocha <ihastealth@gmail.com>
2014-12-12 16:05:08 +0000

    board made

--- terceira entrega/LAIGvs10/src/SceneBoard.cpp
+++ terceira entrega/LAIGvs10/src/SceneBoard.cpp
@@ -11,10 +11,85 @@ SceneBoard::SceneBoard():block()
 void SceneBoard::draw(void)
 {
 	glPushMatrix();
+
+	glPushMatrix();
 	for(unsigned int i = 0; i < 5; i++) {
 		block.draw();
-		glTranslatef(2.0f, 0.0f, 0.0f);
+		glTranslatef(2.0*sin(PI/3.0), 0.0f, 0.0f);
 	}
+	glPopMatrix();
+	
+	glTranslatef(-sin(PI/3.0), 0.0f, 1.5f);
+	glPushMatrix();
+	for(unsigned int i = 0; i < 6; i++) {
+		block.draw();
+		glTranslatef(2.0*sin(PI/3.0), 0.0f, 0.0f);
+	}
+	glPopMatrix();
+		
+	glTranslatef(-sin(PI/3.0), 0.0f, 1.5f);
+
+	glPushMatrix();
+	for(unsigned int i = 0; i < 7; i++) {
+		block.draw();
+		glTranslatef(2.0*sin(PI/3.0), 0.0f, 0.0f);
+	}
+	glPopMatrix();
+
+	glTranslatef(-sin(PI/3.0), 0.0f, 1.5f);
+
+	glPushMatrix();
+	for(unsigned int i = 0; i < 8; i++) {
+		block.draw();
+		glTranslatef(2.0*sin(PI/3.0), 0.0f, 0.0f);
+	}
+	glPopMatrix();
+
+	glTranslatef(-sin(PI/3.0), 0.0f, 1.5f);
+
+	glPushMatrix();
+	for(unsigned int i = 0; i < 9; i++) {
+		block.draw();
+		glTranslatef(2.0*sin(PI/3.0), 0.0f, 0.0f);
+	}
+	glPopMatrix();
+
+	glTranslatef(sin(PI/3.0), 0.0f, 1.5f);
+
+	glPushMatrix();
+	for(unsigned int i = 0; i < 8; i++) {
+		block.draw();
+		glTranslatef(2.0*sin(PI/3.0), 0.0f, 0.0f);
+	}
+	glPopMatrix();
+
+	glTranslatef(sin(PI/3.0), 0.0f, 1.5f);
+
+	glPushMatrix();
+	for(unsigned int i = 0; i < 7; i++) {
+		block.draw();
+		glTranslatef(2.0*sin(PI/3.0), 0.0f, 0.0f);
+	}
+	glPopMatrix();
+
+	glTranslatef(sin(PI/3.0), 0.0f, 1.5f);
+
+	glPushMatrix();
+	for(unsigned int i = 0; i < 6; i++) {
+		block.draw();
+		glTranslatef(2.0*sin(PI/3.0), 0.0f, 0.0f);
+	}
+	glPopMatrix();
+
+	glTranslatef(sin(PI/3.0), 0.0f, 1.5f);
+
+	glPushMatrix();
+	for(unsigned int i = 0; i < 5; i++) {
+		block.draw();
+		glTranslatef(2.0*sin(PI/3.0), 0.0f, 0.0f);
+	}
+	glPopMatrix();
+
 	glPopMatrix();
 }
 
