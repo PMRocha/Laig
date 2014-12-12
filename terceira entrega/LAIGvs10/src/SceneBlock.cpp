3f4cf93a23e04ece007607cdeedd9a8c4a214398
PMRocha <ihastealth@gmail.com>
2014-12-12 15:25:09 +0000

    now it has texture

--- terceira entrega/LAIGvs10/src/SceneBlock.cpp
+++ terceira entrega/LAIGvs10/src/SceneBlock.cpp
@@ -13,17 +13,25 @@ void SceneBlock::draw(void)
 
 	glRotatef(30.0f,0.0f,1.0f,0.0f);
 
+	double texx[6] = {1,0.75,0.25,0,0.25,0.75};
+
+	double texy[6] = {0.5,1,1,0.5,0,0};
+
 	//bases
 	glPushMatrix();
 	glRotatef(90.0f,1.0f,0.0f,0.0f);
 	glBegin(GL_POLYGON);
 	for(int i = 0; i < 6; ++i) {
+		glTexCoord2f(texx[i],texy[i]);
 		glVertex3d(sin(i/6.0*2*PI),cos(i/6.0*2*PI),0);
+		
 	}
 	glEnd();
 	glBegin(GL_POLYGON);
 	for(int i = 6; i > 0; --i) {
+		glTexCoord2f(texx[i-1],texy[i-1]);
 		glVertex3d(sin(i/6.0*2*PI),cos(i/6.0*2*PI),0.5);
+		
 	}
 	glEnd();
 	glPopMatrix();
