30830fb11077af8abdb4b9f2e26c45b6c765b559
PMRocha <ei11078@fe.up.pt>
2015-01-04 18:27:57 +0000

    test

---
+++ terceira entrega/LAIGvs10/src/SceneAmbient.cpp
@@ -0,0 +1,11 @@
+﻿#include "SceneAmbient.h"
+
+SceneAmbient::SceneAmbient():sphere(20,30,30)
+{
+}
+
+void SceneAmbient::draw(){
+	glFrontFace​(GL_CW);
+	sphere.draw();
+	glFrontFace(GL_CCW);
+}
---
+++ terceira entrega/LAIGvs10/src/SceneAmbient.h
@@ -0,0 +1,17 @@
+#ifndef _SCENEAMBIENT_H_
+#define _SCENEAMBIENT_H_
+#include "SceneSphere.h"
+#include <GL/GL.h>
+
+using namespace std;
+
+class SceneAmbient:
+{
+private:
+	SceneSphere sphere;
+public:
+	SceneAmbient();
+	void draw();
+};
+
+#endif
